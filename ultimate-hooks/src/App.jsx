0a215f0995675b01e582ed041a9b9623a8de69dd
nooblue <null>
2024-04-24 11:56:30 +0300

    7.8 implemented useResource

--- ultimate-hooks/src/App.jsx
+++ ultimate-hooks/src/App.jsx
@@ -18,10 +18,18 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(() => {
+    axios.get(baseUrl)
+    .then(res => {
+      setResources(res.data)
+    })
+  }, [])
 
   const create = (resource) => {
-    // ...
+    axios.post(baseUrl, resource)
+    .then(res => {
+      setResources(resources.concat(res.data))
+    })
   }
 
   const service = {
