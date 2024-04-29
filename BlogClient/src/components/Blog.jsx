30379cac8aaca45cd95828d37526cc9619a88f03
nooblue <null>
2024-04-29 11:58:11 +0300

    7.9 BlogClient prettier config

--- BlogClient/src/components/Blog.jsx
+++ BlogClient/src/components/Blog.jsx
@@ -1,49 +1,64 @@
-import { useState, useEffect } from 'react'
-
+import { useState, useEffect } from "react";
 
 const removeButton = (handleRemoveBlog) => {
-  return (<button onClick={handleRemoveBlog}>delete</button>)
-}
+  return <button onClick={handleRemoveBlog}>delete</button>;
+};
 
 const Blog = ({ blog, user, addLike, removeBlog }) => {
-  const [visible, setVisible] = useState(false)
-  const [buttonText, setButtonText] = useState('show')
+  const [visible, setVisible] = useState(false);
+  const [buttonText, setButtonText] = useState("show");
 
   useEffect(() => {
-    setButtonText(visible ? 'hide' : 'show')
-  },[visible])
+    setButtonText(visible ? "hide" : "show");
+  }, [visible]);
 
   const handleVisibility = async (e) => {
-    e.preventDefault()
-    setVisible(!visible)
-  }
+    e.preventDefault();
+    setVisible(!visible);
+  };
 
   const handleLike = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    await addLike({ ...blog, user: blog.user.id, likes: blog.likes + 1 })
-  }
+    await addLike({ ...blog, user: blog.user.id, likes: blog.likes + 1 });
+  };
 
   const handleRemoveBlog = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    if(window.confirm(`Do you want to remove blog '${blog.title}' by '${blog.author}'`))
-    {
-      await removeBlog({ ...blog, user: blog.user.id })
+    if (
+      window.confirm(
+        `Do you want to remove blog '${blog.title}' by '${blog.author}'`,
+      )
+    ) {
+      await removeBlog({ ...blog, user: blog.user.id });
     }
-  }
+  };
 
   return (
-    <div className='blog' style={{ margin: '7px', padding: '4px', border: 'solid', borderWidth: '1px' }}>
-      <div className='blogTitleAuthor'>{blog.title} - {blog.author} <button onClick={handleVisibility}>{buttonText}</button></div>
-      <div name='ExtraBlogInfo' style={{ display: visible ? '' : 'none' }}>
+    <div
+      className="blog"
+      style={{
+        margin: "7px",
+        padding: "4px",
+        border: "solid",
+        borderWidth: "1px",
+      }}
+    >
+      <div className="blogTitleAuthor">
+        {blog.title} - {blog.author}{" "}
+        <button onClick={handleVisibility}>{buttonText}</button>
+      </div>
+      <div name="ExtraBlogInfo" style={{ display: visible ? "" : "none" }}>
         <div>{blog.url}</div>
-        <div className='blogLikes'>likes: {blog.likes} <button onClick={handleLike}>like</button></div>
+        <div className="blogLikes">
+          likes: {blog.likes} <button onClick={handleLike}>like</button>
+        </div>
         <div>uploaded by: {blog.user.name}</div>
         {blog.user.user === user.username && removeButton(handleRemoveBlog)}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
--- BlogClient/src/components/BlogForm.jsx
+++ BlogClient/src/components/BlogForm.jsx
@@ -1,39 +1,67 @@
-import { useState } from 'react'
-
+import { useState } from "react";
 
 const FormValue = ({ label, name, value, setValue }) => {
   const onChangeHelper = (func) => {
-    return ({ target }) => {func(target.value)}
-  }
-
-  return (<div>
-    {label} <input type='text' name={name} value={value} onChange={onChangeHelper(setValue)}></input> <br/>
-  </div>)
-}
-
-const BlogForm = ({ createBlog }) => {
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const [url, setUrl] = useState('')
-
-  const handleBlog = async (e) => {
-    e.preventDefault()
-
-    await createBlog({ title, author, url })
-
-    setTitle('')
-    setAuthor('')
-    setUrl('')
-  }
+    return ({ target }) => {
+      func(target.value);
+    };
+  };
 
   return (
-    <form className='blogForm' onSubmit={handleBlog}>
-      <FormValue key='title' label='title' value={title} name='BlogTitle' setValue={setTitle}></FormValue>
-      <FormValue key='author' label='author' value={author} name='BlogAuthor' setValue={setAuthor}></FormValue>
-      <FormValue key='url' label='url' value={url} name='BlogUrl' setValue={setUrl}></FormValue>
-      <button type='submit'>submit</button>
-    </form>
-  )
-}
+    <div>
+      {label}{" "}
+      <input
+        type="text"
+        name={name}
+        value={value}
+        onChange={onChangeHelper(setValue)}
+      ></input>{" "}
+      <br />
+    </div>
+  );
+};
 
-export default BlogForm
+const BlogForm = ({ createBlog }) => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [url, setUrl] = useState("");
+
+  const handleBlog = async (e) => {
+    e.preventDefault();
+
+    await createBlog({ title, author, url });
+
+    setTitle("");
+    setAuthor("");
+    setUrl("");
+  };
+
+  return (
+    <form className="blogForm" onSubmit={handleBlog}>
+      <FormValue
+        key="title"
+        label="title"
+        value={title}
+        name="BlogTitle"
+        setValue={setTitle}
+      ></FormValue>
+      <FormValue
+        key="author"
+        label="author"
+        value={author}
+        name="BlogAuthor"
+        setValue={setAuthor}
+      ></FormValue>
+      <FormValue
+        key="url"
+        label="url"
+        value={url}
+        name="BlogUrl"
+        setValue={setUrl}
+      ></FormValue>
+      <button type="submit">submit</button>
+    </form>
+  );
+};
+
+export default BlogForm;
